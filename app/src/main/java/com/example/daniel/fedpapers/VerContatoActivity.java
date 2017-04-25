525dafd5ad40fb5679b4a96cebb72e246a3437d2
Daniel Antonio <danielantonio23599@gmail.com>
2017-04-25 19:56:19 -0300

    modificação interface

---
+++ app/src/main/java/com/example/daniel/fedpapers/VerContatoActivity.java
@@ -0,0 +1,13 @@
+package com.example.daniel.fedpapers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerContatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_contato);
+    }
+}
